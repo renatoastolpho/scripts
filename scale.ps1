aae56898975ab4106519c0414c52c9dbf0666b06
renatoastolpho <145033298+renatoastolpho@users.noreply.github.com>
2023-11-23 12:49:36 -0300

    Add files via upload

---
+++ scale.ps1
@@ -0,0 +1,8 @@
+﻿
+$subscription = "Assinatura de Plataformas MSDN"
+$resourceGroup = "Intelectah"
+$appServicePlan = "ASP-Intelectah-b041"
+$sku = "B3" #to basic plan 3
+
+az appservice plan update -name $appServicePlan -resource-group $resourceGroup -subscription $subscription -sku $sku
+az webapp restart -name $appService -resource $resourceGroup -group $resourceGroup -subscription $subscription
